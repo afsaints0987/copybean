8edece19aff6f9a3628693812e3b9abafdaa4c16
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-06 16:44:16 +0800

    update controller

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -37,7 +37,7 @@ const generateSEO = async (req, res) => {
   
           res.status(400).json({
             success: false,
-            error: "Generate Tagline Failed",
+            error: "Generate SEO Failed",
             message: error.message
           });
     }
